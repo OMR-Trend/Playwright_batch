def77351736c6c3c3ade84da4962f82bdae35604
OMR-Trend <nitheesgopi@gmail.com>
2024-11-25 19:36:39 +0530

    first push

---
+++ script.js
@@ -0,0 +1,379 @@
+
+///printing statement
+
+//console.log()
+ 
+// console.log('hello js')
+
+
+
+
+//output : 
+
+//extension - code runner 
+//terminal - node "filename"   
+
+//how to write the js code :  
+
+//syntax : 
+
+//variable_type variable_name = value 
+//console.log(variable_name)
+
+//  const a = 10
+// console.log(a)
+
+
+//variable type : 
+//var => redeclare , reassign
+
+// var a = 10  //redeclare
+//     a = 5   //reassign
+// console.log(a);
+
+
+// //let
+
+// let b = 10  //it can't redeclare
+//     b = 5   //reassign
+// console.log(b);
+
+// //const
+
+// const c = 10   // it can't redeclare
+   
+// console.log(c);  //it can't reassign
+
+
+//Datatypes : 
+
+//primitive datatypes : 
+
+// //string - collection of values in the double Quotes
+// const str = "456@#$%^&"
+// console.log(str);
+
+// //number :  integers , decimal
+
+// const num = 10.5
+// console.log(num);
+
+// //boolean 
+
+// const b = false
+// console.log(b);
+
+
+// //null 
+
+// const empval = null
+// console.log(empval);
+
+
+// //undefined 
+
+// let ud;
+// console.log(ud);
+
+
+// //non - primitive datatypes
+
+// //array - [] 
+// //array is a collection of values and datas within the square bracket
+
+// const arr = ["js",123,12.3,true,null]
+// console.log(arr);
+
+// //dairy milk , 5star,mangobite, munch , perk
+
+// //office employee details - name , emp id , blood grp , mob .no , role 
+
+// //length => starts with 1
+// //index => starts with 0
+
+
+// //object => {  } 
+//     //collection of key and value pairs
+
+//     const obj = {
+
+//        //key : value 
+
+//        name : "santhosh",
+//        age : 39,
+//        dep : "IT"
+
+//     }
+
+//     console.log(obj);
+    
+
+//Operators : 
+
+//arithmetic : add,sub,mul,div,modulus,exponent,increment,decrement 
+
+//add+
+
+// var a = 10 
+// var b = 5
+// console.log(a+b);
+
+// //sub 
+
+// var c = 10
+// var d = 5
+// console.log(c-d);
+
+
+// //mul 
+// var mul1 = 5
+// var mul2 = 5
+// console.log(mul1*mul2);
+
+// //div 
+
+// var div1 = 10
+// var div2 = 5
+// console.log(div1/div2); 
+
+// //modulus 
+
+// var mod1 = 10
+// var mod2 = 5
+// console.log(mod1%mod2);
+
+// //exponent : 
+
+// var exp = 3
+// var exp2 = 5 
+// console.log(exp**exp2);
+
+// //increment
+// var inc = 10
+//  inc++  // 10+1
+//  console.log(inc);
+ 
+//  //decrement : 
+
+//  var dec = 10
+//  dec--
+//  console.log(dec);
+ 
+// //assignment 
+
+// var assn = 10
+
+
+
+// //logical 
+// // && => and 
+// // || => or
+// // ! => not
+
+// var n = 10
+// var n1 = 10
+// console.log(n!=n1);
+// //equality 
+
+// //loose equality 
+
+// // == 
+
+// var num1 = 10
+// var num2 = "10"
+// console.log(num1 == num2);
+
+
+// //strict equality
+
+// //=== 
+
+// var num3 = 30
+// var num4 = "30"
+// console.log(num3 === num4);
+
+// //comparsion 
+
+// //< less than  
+
+// var a = 10
+// var b = 5 
+// console.log(a<b);
+
+
+// // >greater than 
+// var a = 10
+// var b = 5 
+// console.log(a>b);
+// // <= lessthan or equal // >= greaterthan or equal
+ 
+// var c = 50
+// var d = 50 
+// console.log(c>=d);
+
+// //ternary 
+
+
+// //(condition)? "value_true statement" : "value_false statement"
+
+ 
+// var age = 1000
+// var limit = (age>=18 && age<= 70)? "He eligible to vote" : "He is not eligible to vote" 
+// console.log(limit);
+
+
+
+//Task : 
+
+//170 to 185 cm
+
+//function : 
+
+//function statement or declaration  
+
+//syntax : 
+
+//function var_name(){
+
+//code
+
+//}
+
+
+// function box(){
+//     console.log("Im executed");
+// }
+// box()
+
+
+// function newbox(a){ ///parameter
+
+//      console.log(a);
+     
+// }
+// newbox(10) //argument 
+
+
+//return  
+
+// function Person(x){
+
+//     return x
+// }
+
+// //first method
+// console.log(Person("cash"));
+
+// //second method 
+// let storage = Person("cash")
+// console.log(storage);
+
+
+//function expression 
+
+//syntax:
+
+//var_type var_name = function(){
+
+//}
+
+// var newfn = function(x){
+//     console.log(x);
+    
+// }
+// newfn("im function expression")v
+
+// function box(){
+
+//     console.log("Im executed");
+    
+// }
+// box()
+
+
+
+
+//immediate invoke function  
+
+// var a = (function box(x){
+      
+//     console.log(x);
+    
+// })(10)
+
+
+
+
+// //first class function 
+
+// function newfn(x){
+//   return x*x
+// }
+
+// function passfn(y){
+
+//   console.log(" The value is "+y(5)); 
+  
+// } 
+
+// passfn(newfn)  
+
+
+
+ 
+
+// 5x5 == 25 
+
+//3x3 
+
+
+//arrow function
+
+//syntax: 
+
+//()=>{ }  
+
+// var arr = (x) => {console.log(x)}
+// arr("Arrow function")
+
+
+// var a = 10  // intialization
+// console.log(a); //declaration
+
+// var b = 5
+// console.log(b);
+
+// function box(){
+//   var a = 10
+//   console.log(a);
+  
+// }
+
+// box()
+
+//Hoisting :  
+
+// console.log(a);
+// const a = 5
+// console.log(a);
+
+
+//function : 
+
+
+
+//o/p : 
+
+
+//hello "Jai" how are you "?"
+
+//modulus and exponent  
+
+//function expression 
+
+
+
+
+
+
+
